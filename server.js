d06d0e113cd0a85592f44cd5ad9630c47107b722
nishasoms <30654247+nishasoms@users.noreply.github.com>
2017-08-15 01:41:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('Article one is requested ad will be served!!');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send('Article two is requested ad will be served!!');
